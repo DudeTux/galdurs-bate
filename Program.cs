120b89803f213654e7abca5e7ce19e876aeafdbf
RatherShort02 <142900849+RatherShort02@users.noreply.github.com>
2023-08-23 07:46:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace app
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Alter = 18;
+            string username = "Nix";
+
+
+
+            Console.WriteLine("Gib bitte deinen Namen ein:");
+
+            username = Console.ReadLine();
+            
+            Console.WriteLine("Aha Du bist also " + username + " stimmt dass auch? J/N");
+
+            string usernameTest = Console.ReadLine();
+
+            if (usernameTest == "J")
+            {
+                Console.WriteLine("Sehr schön Willst du den Prolog Regeln Skippen (basic DnD)");
+
+                Console.ReadLine();
+            }
+            
+
+
+
+
+
+            
+            
+        }
+    }
+}
