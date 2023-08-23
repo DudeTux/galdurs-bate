53d2ce69285a0071eed0054de4c733366a355b30
Dude Tux <dude.lfl@pm.me>
2023-08-23 11:45:15 +0200

    Würfel hinzugefügt.

---
+++ dice.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zufallszahlengenerator initialisieren
+            Random random = new Random();
+
+            // Würfeln
+            int d4 = random.Next(1, 5); // Werte zwischen 1 und 20
+            int d6 = random.Next(1, 7); // Werte zwischen 1 und 20
+            int d8 = random.Next(1, 9); // Werte zwischen 1 und 20
+            int d10 = random.Next(1, 11); // Werte zwischen 1 und 20
+            int d12 = random.Next(1, 13); // Werte zwischen 1 und 20
+            int d20 = random.Next(1, 21); // Werte zwischen 1 und 20
+
+        }
+    }
+}using System;
