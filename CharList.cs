4c1e021c7b054833fa042e87db0b8b6c3729c4b3
Raphael Schillnmeier <raphi.schillmeier@gmail.com>
2023-08-23 11:50:22 +0000

    alle gegner grunddaten definiert

--- CharList.cs
+++ CharList.cs
@@ -1085,186 +1085,6 @@ internal class CharList
         {   
             Name = "Baldur";
             Hp = 
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 10
-            Friendly = 0
-            Age = 
-            
-            }
-        class Enemy_Boss;
-        {   
-            Name = "Hippogriff";
-            Hp = 19
-            Magic = 0 //unklar
-            Attack = 3
-            Defence = 11
-            Geschicklichkeit = 14
-            Strength = 20
-            Speed = 40
-            Luck = 5
-            Intelligence = 2
-            Race = 0 //unklar, nicht besprochen
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_Boss;
-        {   
-            Name = "Invisible Stalker";
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_middle;
-        {   
-            Name = "Giant Spider"
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-             Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_middle;
-        {   
-            Name = "Goblin Mother"
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_middle;
-        {   
-            Name = "Gladiator";
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-
-            }
-        class Enemy_middle;
-        {   
-            Name = "Gnoll";
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_middle;
-        {   
-            Name = "Goblin";
-            Hp = 
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-        class Enemy_low;
-        {   
-            Name = "Slime";
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-            class Enemy_low;
-        {   
-            Name = "Ratte";
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-             Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-            class Enemy_low;
-        {   
-            Name = "Tiger";
-            Hp =
             Magic = 0; //unklar
             Attack =
             Defence =
@@ -1274,7 +1094,205 @@ internal class CharList
             Luck =
             Intelligence =
             Race =
+            Agression = 10;
+            Friendly = 0;
+            Age = 
+            
+            }
+        class Enemy_Boss;
+        {   
+            Name = "Hippogriff";
+            Hp = 20;
+            Magic = 0; //unklar
+            Attack = 3;
+            Defence = 11;
+            Geschicklichkeit = 14;
+            Strength = 20;
+            Speed = 40;
+            Luck = 7;
+            Intelligence = 2;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_Boss;
+        {   
+            Name = "Invisible Stalker";
+            Hp = 104;
+            Magic = 0; //unklar
+            Attack = 3;
+            Defence = 14;
+            Geschicklichkeit = 14;
+            Strength = 19;
+            Speed = 50;
+            Luck = 11;
+            Intelligence = 10; 
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_middle;
+        {   
+            Name = "Giant Spider";
+            Hp = 26;
+            Magic = 0; //unklar
+            Attack = 4;
+            Defence = 14;
+            Geschicklichkeit = 19; 
+            Strength = 16;
+            Speed = 30;
+            Luck = 1;
+            Intelligence = 0;
+            Race =  0; //unklar, nicht besprochen
              Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_middle;
+        {   
+            Name = "Minotaur Skeleton";
+            Hp = 67;
+            Magic = 0; //unklar
+            Attack = 9;
+            Defence = 12;
+            Geschicklichkeit = 11;
+            Strength = 22;
+            Speed = 40;
+            Luck = 2;
+            Intelligence = 4;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_middle;
+        {   
+            Name = "Gladiator";
+            Hp = 67;
+            Magic = 0; //unklar
+            Attack = 15;
+            Defence = 16;
+            Geschicklichkeit = 17;
+            Strength = 22;
+            Speed = 30;
+            Luck = 17;
+            Intelligence = 10;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+
+            }
+             class Enemy_low;
+        {   
+            Name = "Werewolf";
+            Hp = 58;
+            Magic = 0; //unklar
+            Attack = 9;
+            Defence = 11;
+            Geschicklichkeit = 14;
+            Strength = 17;
+            Speed = 30;
+            Luck = 10;
+            Intelligence = 10;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_middle;
+        {   
+            Name = "Gnoll";
+            Hp = 22;
+            Magic = 0; //unklar
+            Attack = 5;
+            Defence = 15;
+            Geschicklichkeit = 13;
+            Strength = 16;
+            Speed = 30;
+            Luck = 5;
+            Intelligence = 4;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_middle;
+        {   
+            Name = "Goblin";
+            Hp = 7;
+            Magic = 0; //unklar
+            Attack = 2;
+            Defence = 15;
+            Geschicklichkeit = 16;
+            Strength = 7;
+            Speed = 30;
+            Luck = 7;
+            Intelligence = 10;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+        class Enemy_low;
+        {   
+            Name = "Slime";
+            Hp = 2;
+            Magic = 0; //unklar
+            Attack = 5; 
+            Defence = 11;
+            Geschicklichkeit = 13;
+            Strength = 1;
+            Speed = 20;
+            Luck = 0;
+            Intelligence = -2;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+            class Enemy_low;
+        {   
+            Name = "Ratte";
+            Hp = 1;
+            Magic = 0; //unklar
+            Attack = 1;
+            Defence = 10;
+            Geschicklichkeit = 11;
+            Strength = -2;
+            Speed = 20;
+            Luck = 0;
+            Intelligence = 0;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
+            Friendly = 0; //zufall
+            Age = 0; //zufall
+            
+            }
+            class Enemy_low;
+        {   
+            Name = "Tiger";
+            Hp = 37;
+            Magic = 0; //unklar
+            Attack = 5;
+            Defence = 12;
+            Geschicklichkeit = 17;
+            Strength = 20;
+            Speed = 40;
+            Luck = 7;
+            Intelligence = -1;
+            Race = 0; //unklar, nicht besprochen
+            Agression = 0; //zufall
             Friendly = 0; //zufall
             Age = 0; //zufall
             
@@ -1282,16 +1300,16 @@ internal class CharList
             class Enemy_low;
         {   
             Name = "Zombie";
-            Hp =
+            Hp = 22;
             Magic = 0; //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Attack = 3;
+            Defence = 8;
+            Geschicklichkeit = 4;
+            Strength = 14;
+            Speed = 20;
+            Luck = 2;
+            Intelligence = -1;
+            Race = 0; //unklar, nicht besprochen
             Agression = 0; //zufall
             Friendly = 0; //zufall
             Age = 0; //zufall
@@ -1300,16 +1318,16 @@ internal class CharList
             class Enemy_low;
         {   
             Name = "Geier";
-            Hp =
+            Hp = 5;
             Magic = 0; //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Attack = 1;
+            Defence = 10;
+            Geschicklichkeit = 10;
+            Strength = 5;
+            Speed = 10;
+            Luck = 1;
+            Intelligence = -2;
+            Race = 0; //unklar, nicht besprochen
             Agression = 0; //zufall
             Friendly = 0; //zufall
             Age = 0; //zufall
@@ -1317,17 +1335,17 @@ internal class CharList
             }
             class Enemy_low;
         {   
-            Name =
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Name = "Skelet";
+            Hp = 13;
+            Magic = 0; //unklar
+            Attack = 2;
+            Defence = 13;
+            Geschicklichkeit = 16; 
+            Strength = 10;
+            Speed = 30;
+            Luck = 2;
+            Intelligence = 4;
+            Race = 0; //unklar, nicht besprochen
             Agression = 0 //zufall
             Friendly = 0 //zufall
             Age = 0 //zufall
@@ -1335,17 +1353,17 @@ internal class CharList
             }
             class Enemy_low;
         {   
-            Name =
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Name = "Rabe";
+            Hp = 1;
+            Magic = 0; //unklar
+            Attack = 1;
+            Defence = 12;
+            Geschicklichkeit = 16;
+            Strength = -2;
+            Speed = 10;
+            Luck = 4;
+            Intelligence = -2;
+            Race = 0; //unklar, nicht besprochen
              Agression = 0 //zufall
             Friendly = 0 //zufall
             Age = 0 //zufall
@@ -1353,17 +1371,17 @@ internal class CharList
             }
             class Enemy_low;
         {   
-            Name =
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Name = "Scorpion";
+            Hp = 1;
+            Magic = 0; //unklar
+            Attack = 1;
+            Defence = 11;
+            Geschicklichkeit = 11;
+            Strength = -2;
+            Speed = 10;
+            Luck = -2;
+            Intelligence = -4;
+            Race = 0; //unklar, nicht besprochen
              Agression = 0 //zufall
             Friendly = 0 //zufall
             Age = 0 //zufall
@@ -1371,35 +1389,17 @@ internal class CharList
             }
             class Enemy_low;
         {   
-            Name =
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
-            Agression = 0 //zufall
-            Friendly = 0 //zufall
-            Age = 0 //zufall
-            
-            }
-            class Enemy_low;
-        {   
-            Name =
-            Hp =
-            Magic = 0 //unklar
-            Attack =
-            Defence =
-            Geschicklichkeit =
-            Strength =
-            Speed =
-            Luck =
-            Intelligence =
-            Race =
+            Name = "Seepferdchen";
+            Hp = 1;
+            Magic = 0; //unklar
+            Attack = 1;
+            Defence = 11;
+            Geschicklichkeit = 13;
+            Strength = -4;
+            Speed = 0;
+            Luck = -2;
+            Intelligence = -4;
+            Race = 0; //unklar, nicht besprochen
             Agression = 0 //zufall
             Friendly = 0 //zufall
             Age = 0 //zufall
