591e703bb4035b6c71e23e6fe8d9825820b5a083
Dudetux <dude.lfl@pm.me>
2023-08-23 13:56:58 +0200

    fixed comments.

--- dice.cs
+++ dice.cs
@@ -10,11 +10,11 @@ namespace Dice
             Random random = new Random();
 
             // Würfeln
-            int d4 = random.Next(1, 5); // Werte zwischen 1 und 20
-            int d6 = random.Next(1, 7); // Werte zwischen 1 und 20
-            int d8 = random.Next(1, 9); // Werte zwischen 1 und 20
-            int d10 = random.Next(1, 11); // Werte zwischen 1 und 20
-            int d12 = random.Next(1, 13); // Werte zwischen 1 und 20
+            int d4 = random.Next(1, 5); // Werte zwischen 1 und 4
+            int d6 = random.Next(1, 7); // Werte zwischen 1 und 6
+            int d8 = random.Next(1, 9); // Werte zwischen 1 und 8
+            int d10 = random.Next(1, 11); // Werte zwischen 1 und 10
+            int d12 = random.Next(1, 13); // Werte zwischen 1 und 12
             int d20 = random.Next(1, 21); // Werte zwischen 1 und 20
 
         }
