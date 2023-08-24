74691e5341a7213beeca9389de67fc24ca0d7da8
Dudetux <dude.lfl@pm.me>
2023-08-24 08:14:47 +0200

    dice functions with modificator.

--- dice.cs
+++ dice.cs
@@ -1,23 +1,3 @@
-using System;
-
-namespace Dice
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Zufallszahlengenerator initialisieren
-            Random random = new Random();
-
-            // Würfeln
-            int d4 = random.Next(1, 5); // Werte zwischen 1 und 4
-            int d6 = random.Next(1, 7); // Werte zwischen 1 und 6
-            int d8 = random.Next(1, 9); // Werte zwischen 1 und 8
-            int d10 = random.Next(1, 11); // Werte zwischen 1 und 10
-            int d12 = random.Next(1, 13); // Werte zwischen 1 und 12
-            int d20 = random.Next(1, 21); // Werte zwischen 1 und 20
-
-
             public int d4(int mod)
             {
                 static void Main(string[] args)
@@ -58,9 +38,3 @@ namespace Dice
                 static void Main(string[] args)
                            int result = random.Next(1, 21) + mod;
             }
-
-
-
-        }
-    }
-}using System;
