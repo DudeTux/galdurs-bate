968b6297390543344c348ed7539e3a158b32cf6b
Dudetux <dude.lfl@pm.me>
2023-08-24 07:49:01 +0200

    I made functions for the dice to use them more the one time, implemented mod.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Systeming 
 
 namespace app
 {
--- dice.cs
+++ dice.cs
@@ -17,6 +17,50 @@ namespace Dice
             int d12 = random.Next(1, 13); // Werte zwischen 1 und 12
             int d20 = random.Next(1, 21); // Werte zwischen 1 und 20
 
+
+            public int d4(int mod)
+            {
+                static void Main(string[] args)
+                int result = random.Next(1, 5) + mod;
+            }
+
+
+            public int d6(int mod)
+            {
+                static void Main(string[] args)
+                int result = random.Next(1, 7) + mod;
+            }
+
+
+            public int d8(int mod)
+            {
+                static void Main(string[] args)
+                           int result = random.Next(1, 9) + mod;
+            }
+
+
+            public int d10(int mod)
+            {
+                static void Main(string[] args)
+                           int result = random.Next(1, 11) + mod;
+            }
+
+
+            public int d12(int mod)
+            {
+                static void Main(string[] args)
+                           int result = random.Next(1, 13) + mod;
+            }
+
+
+            public int d20(int mod)
+            {
+                static void Main(string[] args)
+                           int result = random.Next(1, 21) + mod;
+            }
+
+
+
         }
     }
 }using System;
