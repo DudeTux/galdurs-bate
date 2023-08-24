6d8e3698ee459ba553633cd61e88bc88e18c5712
raphischillmeier <schillmeierraphael@gmail.com>
2023-08-24 09:59:36 +0200

    update

--- piontSys.cs
+++ piontSys.cs
@@ -1,17 +1,48 @@
-using System
+using System;
 
-namespace functions 
-{
-     class pointBoss 
-     {
+namespace lvlPoints{
 
-        enemy_boss = Enemy_Boss
-        enemy_middle = En
+   public static class Programm{
+      
+            int Hp;
+            int Magic; 
+            int Attack; 
+            int Defence; 
+            int Geschicklichkeit; 
+            int Strength;
+            int Speed;
+            int Luck;
+            int Intelligence; 
+            string Race;
+            string Agression; 
+            string Friendly;
+            int Age;
 
-        static void pointSysBoss(Enemy_Boss) 
-        {
-            
-        }
+      public static void PointsLvlUp()
+      {
 
-     }
+         Console.WriteLine("Welche Charaktereigenschaft möchtest du verbessern?");
+         var Answer = console.Read();
+         GetType();
+
+      }   
+
+      public static void GetType()
+      {
+
+         var Type = answer.typeOf();
+         
+
+      }
+
+      public static string A_F(string wert)
+      {
+
+         if (wert == )
+         {
+            wert = ;
+         }
+         
+      }
+   }
 }
