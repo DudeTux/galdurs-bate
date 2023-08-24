34e9da98fd331ac94978cb4ebc287afa10c078af
raphischillmeier <142599185+raphischillmeier@users.noreply.github.com>
2023-08-24 08:25:20 +0200

    Punktesystem erstellt

---
+++ piontSys.cs
@@ -0,0 +1,17 @@
+using System
+
+namespace functions 
+{
+     class pointBoss 
+     {
+
+        enemy_boss = Enemy_Boss
+        enemy_middle = En
+
+        static void pointSysBoss(Enemy_Boss) 
+        {
+            
+        }
+
+     }
+}
