65e6cf048cf5c00f8a8b77ad77a5ea96dc231c73
RatherShort02 <142900849+RatherShort02@users.noreply.github.com>
2023-08-28 10:55:03 +0200

    Add files via upload

---
+++ Data Für Raffi/Test - Kopie.cs
@@ -0,0 +1,15 @@
+using System;
+using asciis;
+
+
+namespace buildArt
+{ 
+    
+    class Program
+    {
+        static void Main()
+        {
+        AsciiSchluessel asciiKey = new AsciiSchluessel();
+        }
+    }
+}
